d27035aa476a30a4622ac5b4f58f6cdc429edf12
Rauf <srali@usf.edu>
2025-12-21 14:23:14 -0500

    feat: Initialize STM32F401xx project with basic structure
    - Added main.c with an infinite loop in the main function.
    - Included startup_stm32f401xx.S for vector table and reset handler.
    - Implemented minimal system calls in syscall.c for basic I/O operations.
    - Created sysmem.c for memory management and heap allocation.
    - Configured CMake files for project setup and toolchain integration.
    - Added linker script stm32f401xe_flash.ld for memory layout and section definitions.

---
+++ Drivers/stm32f401xe.h
@@ -0,0 +1,15 @@
+/*
+ * stm32f407xe.h
+ *Created on: 12/21/2025
+ * Author: Sheik R. Ali
+*/
+
+#ifndef STM32F401XE_H
+#define STM32F401XE_H
+
+#define AHB1_BASE_ADDR 0X40020000U
+#define AHB2_BASE_ADDR 0x40010000U
+
+
+
+#endif /* STM32F401XE_H */
